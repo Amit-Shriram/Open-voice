a44bb8a36a6f5475f95311a7b760ecd6b0b9d0e9
Amit-Shriram <114796743+Amit-Shriram@users.noreply.github.com>
2024-08-28 13:27:12 +0530

    Update score_main_server.py

--- score_main_server.py
+++ score_main_server.py
@@ -242,7 +242,7 @@ def store_interview_data(uid: str, name: str, msg_info: list):
 # WebSocket for real-time transcription
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-model_id = "openai/whisper-base"
+model_id = "openai/whisper-large-v2"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
